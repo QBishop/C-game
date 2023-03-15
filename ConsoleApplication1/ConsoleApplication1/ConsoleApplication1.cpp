0502a1f5beb9c6015e13293bd66ec466cc4a587f
SBS GAME <SBS GAME@A-08>
2023-03-15 21:42:17 +0900

    최초시작

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a = 12;
+	int b = 5;
+	printf("%d 와 %d 의 합은 %d 입니다.",a,b,a+b);
+	printf("%d 를 %d 로 빼면 %d 입니다.", a, b, a-b);
+	printf("%d 와 %d 를 곱하면 %d 입니다.", a, b, a*b);
+	printf("%d 를 %d 를 나누면 %d 입니다. 나머지는 %d 입니다.",a, b, a/b,a%b);
+	return 0;
+}
