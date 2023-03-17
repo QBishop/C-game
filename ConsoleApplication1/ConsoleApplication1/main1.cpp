d0b8ef492c907592766be9db1febf67a92bafd06
SBS GAME <SBS GAME@A-08>
2023-03-17 21:48:36 +0900

    3.17일

---
+++ ConsoleApplication1/ConsoleApplication1/main1.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	/*int year = 2023;
+	int month = 3;
+	int day = 17;
+	printf("%d-%d-%d", year, month, day);
+	*/
+
+	
+	/*2개의 변수를 만들고, 변수들을 임의의 값으로 초기화 하고 변수값들의 사칙연산 결과를 구해서 결과를 출력하시오.
+	int a = 13;
+	int	b = 3;
+	int q = a + b;
+	int w = a - b;
+	int e = a * b;
+	int r = a / b;
+	int t = a % b;
+	printf("%d + %d = %d\n", a, b, q);
+	printf("%d - %d = %d\n", a, b, w);
+	printf("%d * %d = %d\n", a, b, e);
+	printf("%d / %d = %d나머지는 %d\n", a, b, r,t);*/
+
+
+
+	
+
+
+
+	return 0;
+}
